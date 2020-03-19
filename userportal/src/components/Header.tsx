5d0530fd2835c6f5ee5c79d0ad8c87571eeb72f4
AegeanSea <haohaisea@gmail.com>
2020-03-19 20:05:03 -0300

    load catalogue data

--- userportal/src/components/Header.tsx
+++ userportal/src/components/Header.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
+import React, { useState, useEffect } from 'react'
 import Chip from '@material-ui/core/Chip'
-import FaceIcon from '@material-ui/icons/Face'
 import SearchIcon from '@material-ui/icons/Search'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
 import Slider from './Slider'
+import { getCatalogues } from '../api'
 
 const Header: React.FC = (props: any, state: any) => {
+  const [catalogue, setCatalogue] = useState([])
+  useEffect(() => {
+    loadProducts()
+  }, [])
+  const loadProducts = async () => {
+    const result = await getCatalogues()
+    const { data } = result
+    setCatalogue(data)
+  }
   const handleClick = () => {
     console.info('You clicked the Chip.')
   }
@@ -26,15 +34,16 @@ const Header: React.FC = (props: any, state: any) => {
           </Grid>
         </Grid>
       </div>
-      <Chip
-        size='small'
-        icon={<FaceIcon />}
-        label='Primary Clickable'
-        avatar={<Avatar alt='Natacha' src='/static/images/avatar/1.jpg' />}
-        onClick={handleClick}
-        clickable
-        color='primary'
-      />
+      {catalogue.length > 0 &&
+        catalogue.map((name: string) => (
+          <Chip
+            size='small'
+            label={name}
+            onClick={handleClick}
+            clickable
+            color='primary'
+          />
+        ))}
       <Slider></Slider>
     </div>
   )
