0cb99368391797be762fb3b3da242faf489ffb21
AegeanSea <haohaisea@gmail.com>
2020-04-11 18:53:03 -0300

    address form css adjusted

---
+++ userportal/src/pages/Address.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AddressForm from '../components/AddressForm'
+const Address: React.FC = (props: any, state: any) => {
+  return <AddressForm />
+}
+
+export default Address
--- userportal/src/pages/Home.tsx
+++ userportal/src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ProductList from 'components/ProductList'
+import AddressForm from '../components/AddressForm'
 const Home: React.FC = (props: any, state: any) => {
   return (
     <div>
-      <>
-        <ProductList />
-      </>
+      <ProductList />
     </div>
   )
 }
