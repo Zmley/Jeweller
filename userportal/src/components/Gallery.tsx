87d02f857840eff81afc541043d061f512862f8a
AegeanSea <haohaisea@gmail.com>
2020-05-02 21:16:23 -0300

    gallery components added

---
+++ userportal/src/components/Gallery.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import { Product } from '../models/index'
+import './Gallery.scss'
+import sample from '../assets/img/productSample.png'
+
+export default function TitlebarGridList() {
+  const [products, setProducts] = useState<Array<Product>>([])
+
+  return (
+    <div className='galleryBox'>
+      <GridList cellHeight={116} className='gridList' cols={3}>
+        {products.map(product => (
+          <GridListTile key={product.images[0]}>
+            <img src={product.images[0]} alt={product.name} />
+          </GridListTile>
+        ))}
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+        <GridListTile key={sample}>
+          <img src={sample} />
+        </GridListTile>
+      </GridList>
+    </div>
+  )
+}
--- userportal/src/pages/Favourite.tsx
+++ userportal/src/pages/Favourite.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import Gallery from '../components/Gallery'
 const Favourite: React.FC = (props: any, state: any) => {
-  return <div>favourite</div>
+  return (
+    <div>
+      <Gallery />
+    </div>
+  )
 }
 
 export default Favourite
