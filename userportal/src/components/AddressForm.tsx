f346e57139634e4222c0729d7d14ceb6e1ae6d3c
AegeanSea <haohaisea@gmail.com>
2020-05-09 23:05:50 -0300

    code updated

--- userportal/src/components/AddressForm.tsx
+++ userportal/src/components/AddressForm.tsx
@@ -2,29 +2,22 @@ import React, { useState, useEffect } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
 import './AddressForm.scss'
-import { IconButton } from '@material-ui/core';
-import ArrowRightAltRounded from '@material-ui/icons/ArrowRightAltRounded';
+import { IconButton } from '@material-ui/core'
+import ArrowRightAltRounded from '@material-ui/icons/ArrowRightAltRounded'
 
 const AddressForm: React.FC = (props: any, state: any) => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState();
-  const [address, setAddress] = useState("");
-  const [postcode, setPostcode] = useState("");
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState()
+  const [address, setAddress] = useState('')
+  const [postcode, setPostcode] = useState('')
   const handleSubmit = (event: any) => {
-    event.preventDefault();
+    event.preventDefault()
     const addressInfo = {
       name: name,
       phone: phone,
       address: address,
       postcode: postcode
     }
-    console.log('A value was submitted: ' + name + "," + phone + "," + address + "," + postcode);
-  }
-  const validation = () => {
-    if (!(name && phone && address && postcode)) {
-      return true
-    }
-    return false
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -36,6 +29,7 @@ const AddressForm: React.FC = (props: any, state: any) => {
             placeholder='Placeholder'
             size='small'
             fullWidth
+            required
             value={name}
             onChange={e => setName(e.target.value)}
           />
@@ -45,6 +39,7 @@ const AddressForm: React.FC = (props: any, state: any) => {
             id='phone'
             type='number'
             label='Phone'
+            required
             placeholder='Placeholder'
             size='small'
             fullWidth
@@ -57,6 +52,7 @@ const AddressForm: React.FC = (props: any, state: any) => {
             id='address'
             label='Address'
             size='small'
+            required
             placeholder='Address'
             fullWidth
             value={address}
@@ -69,14 +65,21 @@ const AddressForm: React.FC = (props: any, state: any) => {
             label='Postcode'
             placeholder='Placeholder'
             size='small'
+            required
             fullWidth
             value={postcode}
             onChange={e => setPostcode(e.target.value)}
           />
         </Grid>
         <Grid item xs={12} className='btnField'>
-          <IconButton aria-label="submmit" className='btn' type="submit" value="Submit" disabled={validation()}>
-            <ArrowRightAltRounded fontSize="large" className='submitIcon' />
+          <IconButton
+            aria-label='submmit'
+            className='btn'
+            type='submit'
+            value='Submit'
+            disabled={name && phone && address && postcode ? true : false}
+          >
+            <ArrowRightAltRounded fontSize='large' className='submitIcon' />
           </IconButton>
         </Grid>
       </Grid>
